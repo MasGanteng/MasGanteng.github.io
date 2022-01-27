a114cbfb293b7d76a30b835940cfce51e1fe39cf
MasGanteng <septiansagus@gmail.com>
2022-01-27 12:08:20 +0700

    e

---
+++ components/news/datas.js
@@ -0,0 +1,66 @@
+export const newss = [
+  {
+    image: "red",
+    date: "Tuesday, 12/1/22",
+    title: "The croissant-waffle",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore praesentium, itaque molestiae earum tempora, dolore voluptatibus sit minus quae nisi recusandae porro fugiat deserunt eligendi eius. Similique nulla, perferendis facilis quo quaerat voluptates inventore excepturi, dolores aliquid mollitia nihil.",
+    link: "/pages/news/detail.html",
+  },
+  {
+    image: "yellowcorn",
+    date: "Tuesday, 12/1/22",
+    title: "The croissant-waffle hybrid that’s taking over Jakarta’s cafés",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore praesentium, itaque molestiae earum tempora, dolore voluptatibus sit minus quae nisi recusandae porro fugiat deserunt eligendi eius. Similique nulla, perferendis facilis quo quaerat voluptates inventore excepturi, dolores aliquid mollitia nihil.",
+    link: "/pages/news/detail.html",
+  },
+  {
+    image: "maroon",
+    date: "Tuesday, 12/1/22",
+    title: "The croissant-waffle hybrid that’s taking over Jakarta’s cafés",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore praesentium, itaque molestiae earum tempora, dolore voluptatibus sit minus quae nisi recusandae porro fugiat deserunt eligendi eius. Similique nulla, perferendis facilis quo quaerat voluptates inventore excepturi, dolores aliquid mollitia nihil.",
+    link: "/pages/news/detail.html",
+  },
+  {
+    image: "pink",
+    date: "Tuesday, 12/1/22",
+    title: "The croissant-waffle hybrid that’s taking over Jakarta’s cafés The croissant-waffle hybrid that’s taking over Jakarta’s cafés",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore praesentium, itaque molestiae earum tempora, dolore voluptatibus sit minus quae nisi recusandae porro fugiat deserunt eligendi eius. Similique nulla, perferendis facilis quo quaerat voluptates inventore excepturi, dolores aliquid mollitia nihil.",
+    link: "/pages/news/detail.html",
+  },
+  {
+    image: "green",
+    date: "Tuesday, 12/1/22",
+    title: "The croissant-waffle hybrid that’s taking over Jakarta’s cafés",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore praesentium, itaque molestiae earum tempora, dolore voluptatibus sit minus quae nisi recusandae porro fugiat deserunt eligendi eius. Similique nulla, perferendis facilis quo quaerat voluptates inventore excepturi, dolores aliquid mollitia nihil.",
+    link: "/pages/news/detail.html",
+  },
+  {
+    image: "yellow",
+    date: "Tuesday, 12/1/22",
+    title: "The croissant-waffle hybrid that’s taking over Jakarta’s cafés",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore praesentium, itaque molestiae earum tempora, dolore voluptatibus sit minus quae nisi recusandae porro fugiat deserunt eligendi eius. Similique nulla, perferendis facilis quo quaerat voluptates inventore excepturi, dolores aliquid mollitia nihil.",
+    link: "/pages/news/detail.html",
+  },
+  {
+    image: "blue",
+    date: "Tuesday, 12/1/22",
+    title: "The croissant-waffle hybrid that’s taking over Jakarta’s cafés",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore praesentium, itaque molestiae earum tempora, dolore voluptatibus sit minus quae nisi recusandae porro fugiat deserunt eligendi eius. Similique nulla, perferendis facilis quo quaerat voluptates inventore excepturi, dolores aliquid mollitia nihil.",
+    link: "/pages/news/detail.html",
+  },
+  {
+    image: "purple",
+    date: "Tuesday, 12/1/22",
+    title: "The croissant-waffle hybrid that’s taking over Jakarta’s cafés",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore praesentium, itaque molestiae earum tempora, dolore voluptatibus sit minus quae nisi recusandae porro fugiat deserunt eligendi eius. Similique nulla, perferendis facilis quo quaerat voluptates inventore excepturi, dolores aliquid mollitia nihil.",
+    link: "/pages/news/detail.html",
+  },
+];
---
+++ components/news/index.js
@@ -0,0 +1,22 @@
+import { newss } from "./datas.js";
+
+const newsLists = document.querySelector(".news-lists");
+
+export const careerAble = () => {
+  newss.map((news) => {
+    var { image, date, title, content, link } = news;
+    newsLists.innerHTML += `
+        <li>
+            <figure>
+                <img src="https://source.unsplash.com/random/${image}" alt="${title}" title="${title}" />
+                <figcaption>
+                    <p class="date">${date}</p>
+                    <p class="title">${title}</p>
+                    <p class="content">${content.slice(0,300)}</p>
+                </figcaption>
+                <a href="${link}">See Detail News</a>
+            </figure>
+        </li>
+    `;
+  });
+};
