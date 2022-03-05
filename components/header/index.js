2cf069bf0781fee57c410ec33f9bd1cd2f851a8d
MasGanteng <septiansagus@gmail.com>
2022-03-05 19:07:16 +0700

    k

--- components/header/index.js
+++ components/header/index.js
@@ -1,22 +1,40 @@
 import { navs } from "./datas.js";
 
 export const navList = () => {
+  const header = document.querySelector(".my-header")
   const navbar = document.querySelector(".navbar > ul");
-  navs.map((nav) => {
-    const { navText, navLink, navActive:active="" } = nav;
+  // navs.map((nav) => {
+  //   const { navText, navLink, navActive:active="" } = nav;
     
-    navbar.innerHTML += `
-        <li>
-            <a 
-                href="${navLink}"
-                >${navText}</a
-            >
-        </li>    
-    `;
-  });
+  //   navbar.innerHTML += `
+  //       <li>
+  //           <a 
+  //               href="${navLink}"
+  //               >${navText}</a
+  //           >
+  //       </li>    
+  //   `;
+  // });
+
+  header.innerHTML += `
+    <div class="container">
+      <nav class="navbar">
+        <a href="/">
+            <img src="/assets/images/logo/logo.png" alt="Logo">
+        </a>
+        <ul>
+          ${navs.map(nav=>`
+          <li>
+            <a href="${nav.navLink}">${nav.navText}</a>
+          </li>
+          `).join("")}
+        </ul>
+      </nav>
+    </div>
+  `
   const activePage = location.href;
   // console.log(activePage);
-  const activeLink = navbar.querySelectorAll('a')
+  const activeLink = document.querySelectorAll('a')
 
   activeLink.forEach(active=>{
     if(active.href === activePage){
