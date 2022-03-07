371ea01c1945933f763ad56ff26bbe7aa3054cc0
MasGanteng <septiansagus@gmail.com>
2022-03-08 04:11:20 +0700

    s

--- components/header/datas.js
+++ components/header/datas.js
@@ -7,30 +7,37 @@ export const navs = [
   {
     navText: "Menu",
     navLink: "/pages/menu",
+    navIcon: "menu"
     // navActive: "active",
   },
   {
     navText: "Partnership",
     navLink: "/pages/partnership",
+    navIcon: "partnership"
   },
   {
     navText: "Outlets",
     navLink: "/pages/outlets",
+    navIcon: "cart"
   },
   {
     navText: "News",
     navLink: "/pages/news",
+    navIcon: "folder"
   },
   {
     navText: "Career",
     navLink: "/pages/career",
+    navIcon: "career"
   },
   {
     navText: "Contact",
     navLink: "/pages/contact",
+    navIcon: "contact"
   },
   {
     navText: "About",
     navLink: "/pages/about",
+    navIcon: "info"
   },
 ];
--- components/header/index.js
+++ components/header/index.js
@@ -17,7 +17,7 @@ export const navList = () => {
   // });
 
   header.innerHTML += `
-    <div class="container">
+    <div class="container view-desktop">
       <nav class="navbar">
         <a href="/">
             <img src="/assets/images/logo/logo.png" alt="Logo">
@@ -31,6 +31,107 @@ export const navList = () => {
         </ul>
       </nav>
     </div>
+
+
+    <div class="view-desktop">
+        <header>
+            <nav class="navbar container">
+                <a href="/">
+                    <img src="/assets/images/logo/logo.png" alt="Logo">
+                </a>
+                <ul></ul>
+            </nav>
+        </header>
+    </div>
+
+    <div class="view-mobile">
+        <header>
+            <nav class="navbar container">
+                <a href="/" class="logo-mobile">
+                    <img src="/assets/images/logo/logo.png" alt="Logo">
+                </a>
+                <a class="toggle-menu" data-toggle="nav-menu">
+                    <svg class="icon">
+                        <use xlink:href="/assets/images/svg/utility-sprite.svg#burger"></use>
+                    </svg>
+                </a>
+            </nav>
+        </header>
+    </div>
+
+
+    <div id="nav-menu" class="nav-menu">
+      <div class="container">
+          <ul class="nav-links-mobile">
+              <li>
+                  <a href="/" class="nav-link">
+                      <svg class="icon">
+                          <use xlink:href="/assets/images/svg/nav-sprite.svg#home"></use>
+                      </svg>
+                      <span>Home</span>
+                  </a>
+              </li>
+            ${navs.map(nav=>`
+              <li>
+                  <a href="${nav.navLink}" class="nav-link">
+                      <svg class="icon">
+                          <use xlink:href="/assets/images/svg/nav-sprite.svg#${nav.navIcon}"></use>
+                      </svg>
+                      <span>${nav.navText}</span>
+                  </a>
+              </li>
+            `).join("")}
+          </ul> <!-- Navigation -->
+
+          <hr>
+
+          <ul class="nav-links-sosmed">
+              <li>
+                  <a href="" class="nav-link">
+                      <svg class="icon">
+                          <use xlink:href="/assets/images/svg/socmed-sprite.svg#facebook-square"></use>
+                      </svg>
+                      <span>Facebook</span>
+                  </a>
+              </li>
+              <li>
+                  <a href="" class="nav-link">
+                      <svg class="icon">
+                          <use xlink:href="/assets/images/svg/socmed-sprite.svg#instagram"></use>
+                      </svg>
+                      <span>Instagram</span>
+                  </a>
+              </li>
+          </ul> <!-- Sosmed -->
+
+          <hr>
+
+          <div>
+              <p class="font-smaller">Shop Partnership</p>
+              <ul class="nav-partner">
+                  <li>
+                      <svg class="icon">
+                          <use xlink:href="/assets/images/svg/socmed-sprite.svg#go-food"></use>
+                      </svg>
+                  </li>
+                  <li>
+                      <svg class="icon">
+                          <use xlink:href="/assets/images/svg/socmed-sprite.svg#shopee-food"></use>
+                      </svg>
+                  </li>
+                  <li>
+                      <svg class="icon">
+                          <use xlink:href="/assets/images/svg/socmed-sprite.svg#grab-food"></use>
+                      </svg>
+                  </li>
+              </ul>
+          </div> <!-- Partnership -->
+
+          <div class="content text-center font-smaller">
+              <p>copyright &copy; 2022 DB's Group</p>
+          </div> <!-- Copyright -->
+      </div>
+    </div>
   `
   const activePage = location.href;
   // console.log(activePage);
@@ -40,4 +141,16 @@ export const navList = () => {
     if(active.href === activePage){
       active.classList.add('active');
     }})
+
+
+    const 
+    dataToggle = document.querySelector("[data-toggle]"),
+    navMenu = document.querySelector("#nav-menu")
+
+dataToggle.addEventListener("click", function(){
+    navMenu.classList.toggle("active")
+})
+if(navMenu.classList.contains('active')){
+    console.log('bgf');
+}
 };
